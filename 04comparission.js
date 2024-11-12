eba181a43d26ca3bfb8ebf2f7b53b4cac393652d
Abhilash PVS <abhilashglam@gmail.com>
2024-11-12 15:29:20 +0530

    Compariision oper

--- 04comparission.js
+++ 04comparission.js
@@ -11,9 +11,9 @@ let b = 5
 /*
 console.log( null > 0 ) //return False
 console.log(null == 0); //return False
-console.log(null <= 0); //return False
+console.log(null <= 0); //return True here null is converted to 0
 
-*/  
 console.log(undefined > 0);
 console.log(undefined == 0);
 console.log(undefined < 0);
+*/  
